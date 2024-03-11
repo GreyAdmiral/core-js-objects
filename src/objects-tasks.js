76839e3a70d5906f897b8a8762e2425cfd301836
Binarion <itmaster@duck.com>
2024-03-11 04:05:28 +0300

    feat: task 8

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -223,8 +223,10 @@ function sellTickets(queue) {
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-   throw new Error('Not implemented');
+function Rectangle(width, height) {
+   this.width = width;
+   this.height = height;
+   this.getArea = () => this.width * this.height;
 }
 
 /**
